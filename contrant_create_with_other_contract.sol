885cf6dc5c8671489fe6e6574ebb8b4f9c405da9
ManikandanInnblockchain <110963828+ManikandanInnblockchain@users.noreply.github.com>
2022-08-12 12:55:07 +0530

    Create contrant_create_with_other_contract.sol

---
+++ contrant_create_with_other_contract.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract Biscut {
+    string public name;
+    address public owner;
+    uint public price;
+
+    constructor(string memory _name, uint _price) payable {
+        name = _name;
+        price = _price;
+        owner = address(this);
+    }
+}
+
+contract biscutFactory {
+    Biscut[] public bis;
+
+    function create(string memory _name, uint _price) public {
+        Biscut biscut = new Biscut(_name, _price);
+        bis.push(biscut);
+    }
+
+    function createWithEther(string memory _name, uint _price, uint _val) public payable {
+        Biscut biscut = (new Biscut){value : _val}(_name, _price);
+        bis.push(biscut);
+    }
+}
